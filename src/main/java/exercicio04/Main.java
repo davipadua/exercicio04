09b2e820e57485c4ec719b633c8085c6c0ed4674
davipadua <davipadua1@gmail.com>
2021-03-09 22:01:48 -0300

    add restante dos arquivos

---
+++ src/main/java/exercicio04/Main.java
@@ -0,0 +1,42 @@
+package exercicio04;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int i = 1;
+//		while(i <= 20) {
+//			System.out.println(i);
+//			i++;
+//			
+//		}
+//		
+//		i = 1;
+//		do {
+//			System.out.println(i);
+//			i++;
+//		} while(i<=20);
+		
+		
+		for(int j = 1; j <= 20; j++) {
+			System.out.println(j);
+		}
+		
+	}
+	
+	
+
+}
